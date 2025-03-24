b5bd6fe3e08d1fbed21437db38d21a09081113eb
Dobiko <kobidogao@yahoo.com>
2025-03-24 16:00:35 +0100

    Add webbuild

---
+++ webbuild/wasm-out/bevy-particles_bg.wasm.d.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: (a: number, b: number) => number;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_1: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_export_6: WebAssembly.Table;
+export const _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hfc412ff13cd5938b: (a: number, b: number, c: number) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h8b280d987bd5a29e: (a: number, b: number) => void;
+export const closure3132_externref_shim: (a: number, b: number, c: any) => void;
+export const closure3135_externref_shim: (a: number, b: number, c: any, d: any) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h1732fd1d0ae2a0f4: (a: number, b: number) => void;
+export const closure99194_externref_shim: (a: number, b: number, c: any) => void;
+export const __wbindgen_start: () => void;
